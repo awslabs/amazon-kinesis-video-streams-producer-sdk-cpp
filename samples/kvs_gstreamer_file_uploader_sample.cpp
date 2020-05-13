16108bba8c747d969dd2f0a57e8d33579ed8ec2b
Lukas Herman <lherman.cs@gmail.com>
2020-05-13 17:21:32 -0400

    Reduce available properties for file uploader sample
    * Remove unnecessary properties
    * Remove redudant properties
    * Convert stream name and file start time to be arguments

--- samples/kvs_gstreamer_file_uploader_sample.cpp
+++ samples/kvs_gstreamer_file_uploader_sample.cpp
@@ -17,6 +17,7 @@ int gstreamer_init(int, char **);
 #endif
 
 #define DEFAULT_RETRY_COUNT       3
+#define DEFAULT_STREAMING_TYPE    "offline"
 
 #define PROPERTY_PREFIX           "KVS_"
 #define PROPERTY_KEY_MAX_LEN      32
@@ -35,24 +36,9 @@ int gstreamer_init(int, char **);
   do { fprintf(stderr, "[ERROR] " APP_NAME ": " fmt "\n", ##__VA_ARGS__); } while(0)
 
 static const char* AVAILABLE_PROPERTIES[] = {
-  PROPERTY_PREFIX "ABSOLUTE_FRAGMENT_TIMES", "Use absolute fragment time",
-  PROPERTY_PREFIX "ACCESS_KEY", "AWS Access Key",
-  PROPERTY_PREFIX "AVG_BANDWIDTH_BPS", "Average bandwidth bps",
-  PROPERTY_PREFIX "AWS_REGION", "AWS Region",
-  PROPERTY_PREFIX "BUFFER_DURATION", "Buffer duration. Unit: seconds",
-  PROPERTY_PREFIX "FILE_START_TIME", "Epoch time that the file starts in kinesis video stream. By default, current time is used. Unit: Seconds",
-  PROPERTY_PREFIX "FRAGMENT_ACKS", "Do fragment acks",
-  PROPERTY_PREFIX "FRAGMENT_DURATION", "Fragment Duration. Unit: miliseconds",
-  PROPERTY_PREFIX "FRAMERATE", "Framerate",
   PROPERTY_PREFIX "IOT_CERTIFICATE", "Use aws iot certificate to obtain credentials",
-  PROPERTY_PREFIX "MAX_LATENCY", "Max Latency. Unit: seconds",
   PROPERTY_PREFIX "RETENTION_PERIOD", "Length of time stream is preserved. Unit: hours",
-  PROPERTY_PREFIX "ROTATION_PERIOD", "Rotation Period. Unit: seconds",
-  PROPERTY_PREFIX "SECRET_KEY", "AWS Secret Key",
-  PROPERTY_PREFIX "STREAM_NAME", "Name of the destination stream",
-  PROPERTY_PREFIX "STREAMING_TYPE", "Streaming type",
   PROPERTY_PREFIX "STORAGE_SIZE", "Storage Size. Unit: MB",
-  PROPERTY_PREFIX "TIMECODE_SCALE", "Timecode Scale. Unit: milliseconds",
   NULL
 };
 
@@ -174,7 +160,7 @@ int gstreamer_init(int argc, char* argv[], CustomData *data) {
     return 0;
 }
 
-string build_kvssink_str() {
+string build_kvssink_str(string stream_name, unsigned long file_start_time) {
     const char** property;
     stringstream ss;
     const char *key_raw, *value;
@@ -182,7 +168,10 @@ string build_kvssink_str() {
     char key[PROPERTY_KEY_MAX_LEN + 1];
     char *ch;
 
-    ss << KVS_SINK_PLUGIN_NAME;
+    ss << KVS_SINK_PLUGIN_NAME
+       << " stream-name=" << stream_name
+       << " streaming-type=" DEFAULT_STREAMING_TYPE  
+       << " file-start-time=" << file_start_time;
     for (property = AVAILABLE_PROPERTIES; *property != NULL; property += 2) {
         key_raw = property[0];
         value = getenv(key_raw);
@@ -212,11 +201,24 @@ void print_usage(char* program_path) {
     memset(padding, ' ', PROPERTY_KEY_MAX_LEN + 1);
 
     printf(
-        "Usage\n"
-        "    %1$s <path/to/file.mp4> [video-only|audio-video]\n\n"
-        "Example\n"
-        "    KVS_MAX_LATENCY=60 %1$s video.mp4\n\n"
-        "Available Properties\n", program_path);
+        "USAGE\n"
+        "    %1$s <stream_name> <file_path> <start_time> [video-only|audio-video]\n\n"
+        "EXAMPLE\n"
+        "    With AWS Credentials\n"
+        "        AWS_ACCESS_KEY_ID=<access_key> AWS_SECRET_ACCESS_KEY=<secret_key> %1$s stream-sample video.mp4 0\n\n"
+        "    With AWS IOT Credentials\n"
+        "        IOT_CERTIFICATE=\"iot-certificate,endpoint=endpoint,cert-path=/path/to/certificate,key-path=/path/to/private/key,ca-path=/path/to/ca-cert,role-aliases=role-aliases\" \\\n"
+        "            %1$s stream-sample video.mp4 0\n\n"
+        "ARGUMENTS\n"
+        "    stream_name     Name of the destination stream\n"
+        "    file_path       A path to a file that you want to upload\n"
+        "    start_time      Epoch time that the file starts in kinesis video stream. If zero, current time is used. Unit: Seconds\n"
+        "    content_type    Optional. Either \"video-only\" or \"audio-video\"\n\n"
+        "AVAILABLE PROPERTIES\n"
+        "    AWS_DEFAULT_REGION              AWS Region\n"
+        "    AWS_ACCESS_KEY_ID               AWS Access Key\n"
+        "    AWS_SECRET_ACCESS_KEY           AWS Secret Key\n"
+        "    AWS_SESSION_TOKEN               AWS Session Token\n", program_path);
 
     for (property = AVAILABLE_PROPERTIES; *property != NULL; property += 2) {
         spaces = PROPERTY_KEY_MAX_LEN - strlen(property[0]);
@@ -230,7 +232,7 @@ void print_usage(char* program_path) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 2 || argc > 3) {
+    if (argc < 4 || argc > 5) {
         print_usage(argv[0]);
         return 1;
     }
@@ -240,20 +242,37 @@ int main(int argc, char* argv[]) {
     int retry_count = DEFAULT_RETRY_COUNT;
     int stream_status = STREAM_STATUS_OK;
     bool do_retry = true;
+    char *ptr;
+    FILE *file;
 
     if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
         print_usage(argv[0]);
         return 1;
     }
-    data.file_path = argv[1];
-    data.kvssink_str = build_kvssink_str();
+    string stream_name = argv[1];
+    data.file_path = argv[2];
+    // checking if fopen returns null seems to be the most portable way to check
+    // file doesn't exist
+    if ((file = fopen(data.file_path.c_str(), "r")) == NULL) {
+        LOG_ERROR("File not found. Path: %s", data.file_path.c_str());
+        return 1;
+    }
+    fclose(file);
+
+    unsigned long file_start_time = strtoul(argv[3], &ptr, 10);
+    if (*ptr != '\0') {
+        LOG_ERROR("File start time is not a valid number");
+        return 1;
+    }
+    data.kvssink_str = build_kvssink_str(stream_name, file_start_time);
+    LOG_INFO("Built kvssink pipeline string: \"%s\"", data.kvssink_str.c_str());
     data.content_type = CONTENT_TYPE_VIDEO_ONLY;
 
-    if (argc > 2) {
-        if (strcmp(argv[2], "audio-video") == 0) {
+    if (argc > 4) {
+        if (strcmp(argv[4], "audio-video") == 0) {
             LOG_INFO("Uploading audio and video");
             data.content_type = CONTENT_TYPE_AUDIO_VIDEO;
-        } else if (strcmp(argv[2], "video-only") == 0) {
+        } else if (strcmp(argv[4], "video-only") == 0) {
             LOG_INFO("Uploading video only");
         } else {
             LOG_INFO("Unrecognized upload type. Default to video-only");
