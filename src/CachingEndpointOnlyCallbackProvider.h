c28332f636444b804a86ec718e5c34608472126d
Mushegh Malkhasyan <musheghm@amazon.com>
2021-02-16 19:36:49 -0800

    Updating CProducer to latest. Add constructor overloads. Add test coverage for reset and re-create

--- src/CachingEndpointOnlyCallbackProvider.h
+++ src/CachingEndpointOnlyCallbackProvider.h
@@ -40,10 +40,10 @@ public:
             std::unique_ptr <CredentialProvider> credentials_provider =
             (std::unique_ptr<EmptyCredentialProvider>) new EmptyCredentialProvider(),
             const std::string &region = DEFAULT_AWS_REGION,
-            const std::string &control_plane_uri = "",
-            const std::string &user_agent_name = "",
-            const std::string &custom_user_agent = "",
-            const std::string &cert_path = "",
+            const std::string &control_plane_uri = EMPTY_STRING,
+            const std::string &user_agent_name = EMPTY_STRING,
+            const std::string &custom_user_agent = EMPTY_STRING,
+            const std::string &cert_path = EMPTY_STRING,
             std::chrono::duration<uint64_t> caching_update_period = std::chrono::seconds(DEFAULT_ENDPOINT_CACHE_UPDATE_PERIOD / HUNDREDS_OF_NANOS_IN_A_SECOND));
 
     virtual ~CachingEndpointOnlyCallbackProvider();
