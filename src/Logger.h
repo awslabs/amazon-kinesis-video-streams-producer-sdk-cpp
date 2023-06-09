e110377cf09e415b51630c7bec5a53640c7ec011
Divya Sampath Kumar <disa6302@colorado.edu>
2023-06-09 12:28:16 -0700

    Logger 12

--- src/Logger.h
+++ src/Logger.h
@@ -38,7 +38,7 @@ namespace com { namespace amazonaws { namespace kinesis { namespace video {
 #endif
 
 #define _LOG_ROLLING_FILE(level, fileName, fileSize, backupIndex, createDirs) \
-    log4cplus::helpers::SharedObjectPtr<log4cplus::Appender> _appender(new log4cplus::RollingFileAppender(fileName, fileSize, backupIndex, createDirs)); \
+    log4cplus::helpers::SharedObjectPtr<log4cplus::Appender> _appender(new log4cplus::RollingFileAppender(fileName, fileSize, backupIndex, true, createDirs)); \
     _appender->setLayout(std::move(std::unique_ptr<log4cplus::PatternLayout>(new log4cplus::PatternLayout("[%-5p] [%d{%d-%m-%Y %H:%M:%S:%Q %Z}] [%-l][%t]")))); \
     log4cplus::BasicConfigurator::doConfigure(log4cplus::Logger::getDefaultHierarchy(), logToStdErr);                                                    \
     log4cplus::Logger::getRoot().addAppender(_appender); \
