5f65b9d1c061f7eca11fb0ead3514be94f99bfb2
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2024-03-04 12:32:23 -0800

    Update build_windows.bat

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem" /v LongPathsEnabled /t REG_DWORD /d 1 /f
 mkdir build
 cd build
