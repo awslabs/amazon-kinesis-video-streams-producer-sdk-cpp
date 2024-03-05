f3f08b50cd3f76e2aa47c38d5365dc9e4b3ba7ff
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2024-03-05 11:27:10 -0800

    Update build_windows.bat

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,7 +1,4 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem" /v LongPathsEnabled /t REG_DWORD /d 1 /f
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" ` -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
-git config --system core.longpaths true
 mkdir build
 cd build
 cmd.exe /c cmake -G "NMake Makefiles" ..
