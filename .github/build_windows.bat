ee9d366fd852183661451d0e6c44c5650a52049f
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2024-04-18 22:33:30 -0700

    Set -DPKG_CONFIG_EXECUTABLE in windows bat

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -2,5 +2,5 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Buil
 mkdir build
 cd build
 cmd.exe /c cmake -G "NMake Makefiles" ..
-cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE -DBUILD_GSTREAMER_PLUGIN=TRUE ..
+cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE -DBUILD_GSTREAMER_PLUGIN=TRUE -DPKG_CONFIG_EXECUTABLE="D:\\gstreamer\\1.0\\msvc_x86_64\\bin\\pkg-config.exe" ..
 nmake
