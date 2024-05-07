61f46d4a4dc860a7588054b698135567fcf752a2
ec2-user <akalanka@amazon.com>
2024-05-07 05:21:51 +0000

    Added img_gen parameter for setting the image generation tags to the video stream to enable S3 image delivery

--- src/gstreamer/gstkvssink.h
+++ src/gstreamer/gstkvssink.h
@@ -57,16 +57,10 @@ G_BEGIN_DECLS
   (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_KVS_SINK))
 #define GST_KVS_SINK_CAST(obj) ((GstKvsSink *)obj)
 
-#ifdef CPP_VERSION_STRING
-#define KVSSINK_USER_AGENT_POSTFIX_VERSION CPP_VERSION_STRING
-#else
-#define KVSSINK_USER_AGENT_POSTFIX_VERSION "UNKNOWN"
-#endif
 
 typedef struct _GstKvsSink GstKvsSink;
 typedef struct _GstKvsSinkClass GstKvsSinkClass;
 typedef struct _KvsSinkCustomData KvsSinkCustomData;
-typedef struct _KvsSinkMetric KvsSinkMetric;
 
 /* all information needed for one track */
 typedef struct _GstKvsSinkTrackData {
@@ -94,7 +88,6 @@ struct _GstKvsSink {
 
     // Stream definition
     gchar                       *stream_name;
-    gchar                       *user_agent;
     guint                       retention_period_hours;
     gchar                       *kms_key_id;
     STREAMING_TYPE              streaming_type;
@@ -108,7 +101,6 @@ struct _GstKvsSink {
     gboolean                    fragment_acks;
     gboolean                    restart_on_error;
     gboolean                    recalculate_metrics;
-    gboolean                    allow_create_stream;
     gboolean                    disable_buffer_clipping;
     guint                       framerate;
     guint                       avg_bandwidth_bps;
@@ -119,14 +111,10 @@ struct _GstKvsSink {
     gchar                       *track_name;
     gchar                       *access_key;
     gchar                       *secret_key;
-    gchar                       *session_token;
     gchar                       *aws_region;
     guint                       rotation_period;
     gchar                       *log_config_path;
     guint                       storage_size;
-    guint                       stop_stream_timeout;
-    guint                       service_connection_timeout;
-    guint                       service_completion_timeout;
     gchar                       *credential_file_path;
     GstStructure                *iot_certificate;
     GstStructure                *stream_tags;
@@ -139,16 +127,14 @@ struct _GstKvsSink {
     guint                       num_audio_streams;
     guint                       num_video_streams;
 
-
     std::unique_ptr<Credentials> credentials_;
     std::shared_ptr<KvsSinkCustomData> data;
+
+    gboolean                    img_gen;
 };
 
 struct _GstKvsSinkClass {
     GstElementClass parent_class;
-    void (*sink_fragment_ack)              (GstKvsSink *kvssink, gpointer user_data);
-    void (*sink_stream_metric)             (GstKvsSink *kvssink, gpointer user_data);
-    void (*sink_stream_error)              (GstKvsSink *kvssink, gpointer user_data);
 };
 
 GType gst_kvs_sink_get_type (void);
@@ -163,26 +149,17 @@ struct _KvsSinkCustomData {
             pts_base(0),
             media_type(VIDEO_ONLY),
             first_video_frame(true),
-            use_original_pts(false),
-            get_metrics(false),
-            on_first_frame(true),
             frame_count(0),
             first_pts(GST_CLOCK_TIME_NONE),
-            producer_start_time(GST_CLOCK_TIME_NONE),
-            streamingStopped(false) {}
+            producer_start_time(GST_CLOCK_TIME_NONE) {}
     std::unique_ptr<KinesisVideoProducer> kinesis_video_producer;
     std::shared_ptr<KinesisVideoStream> kinesis_video_stream;
 
     std::unordered_set<uint64_t> track_cpd_received;
-    GstKvsSink *kvs_sink = nullptr;
+    GstKvsSink *kvsSink;
     MediaType media_type;
     bool first_video_frame;
-    bool use_original_pts;
-    bool get_metrics;
     uint32_t frame_count;
-    bool on_first_frame;
-    std::atomic<bool> streamingStopped;
-    uint64_t frame_pts;
 
     std::atomic_uint stream_status;
 
@@ -190,21 +167,6 @@ struct _KvsSinkCustomData {
     uint64_t pts_base;
     uint64_t first_pts;
     uint64_t producer_start_time;
-    guint err_signal_id = 0;
-    guint ack_signal_id = 0;
-    guint metric_signal_id = 0;
-    uint64_t start_time;  // [nanoSeconds]
-};
-
-struct _KvsSinkMetric {
-    _KvsSinkMetric():
-        frame_pts(0),
-        on_first_frame(true)
-        {}
-    KinesisVideoStreamMetrics stream_metrics = KinesisVideoStreamMetrics();
-    KinesisVideoProducerMetrics client_metrics = KinesisVideoProducerMetrics();
-    uint64_t frame_pts;
-    bool on_first_frame;
 };
 
 #endif /* __GST_KVS_SINK_H__ */
