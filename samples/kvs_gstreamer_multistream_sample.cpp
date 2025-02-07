a07b13f9ba508e9e85a5966f68f43e73ae2ebe8e
sirknightj <jggunawa@amazon.com>
2025-02-06 22:10:13 -0800

    Adding sample checks & invalid dts fix

--- samples/kvs_gstreamer_multistream_sample.cpp
+++ samples/kvs_gstreamer_multistream_sample.cpp
@@ -53,6 +53,7 @@ LOGGER_TAG("com.amazonaws.kinesis.video.gstreamer");
 #define DEFAULT_BUFFER_SIZE (1 * 1024 * 1024)
 #define DEFAULT_STORAGE_SIZE (128 * 1024 * 1024)
 #define DEFAULT_ROTATION_TIME_SECONDS 3600
+#define DEFAULT_FRAME_DURATION_MS 2
 
 namespace com { namespace amazonaws { namespace kinesis { namespace video {
 
@@ -180,6 +181,7 @@ typedef struct _CustomData {
     // Pts of first frame
     map<string, uint64_t> first_pts_map;
     map<string, uint64_t> producer_start_time_map;
+    map<string, uint64_t> last_dts_map;
 } CustomData;
 
 void create_kinesis_video_frame(Frame *frame, const nanoseconds &pts, const nanoseconds &dts, FRAME_FLAGS flags,
@@ -258,6 +260,12 @@ static GstFlowReturn on_new_sample(GstElement *sink, CustomData *data) {
         }
 
         buffer->pts += data->producer_start_time_map[stream_handle_key] - data->first_pts_map[stream_handle_key];
+
+        if (!GST_BUFFER_DTS_IS_VALID(buffer)) {
+            buffer->dts = data->last_dts_map[stream_handle_key] + DEFAULT_FRAME_DURATION_MS * HUNDREDS_OF_NANOS_IN_A_MILLISECOND * DEFAULT_TIME_UNIT_IN_NANOS;
+        }
+
+        data->last_dts_map[stream_handle_key] = buffer->dts;
 
         if (false == put_frame(data->kinesis_video_stream_handles[stream_handle_key], data->frame_data_map[stream_handle_key], buffer_size, std::chrono::nanoseconds(buffer->pts),
                                std::chrono::nanoseconds(buffer->dts), kinesis_video_flags)) {
@@ -405,7 +413,8 @@ int gstreamer_init(int argc, char *argv[]) {
     data.frame_data_map = map<string, uint8_t*>();
     data.frame_data_size_map = map<string, UINT32>();
     data.first_pts_map = map<string, uint64_t>();
-    data.producer_start_time_map = map<string, uint64_t>();;
+    data.producer_start_time_map = map<string, uint64_t>();
+    data.last_dts_map = map<string, uint64_t>();;
 
     /* init GStreamer */
     gst_init(&argc, &argv);
